4282d9db0cc5bdea1215c3acb8ca2f2413e74331
Vitaliy Burkovsky <bvitaliy1@gmail.com>
2019-12-30 23:23:59 +0200

    some fix

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -13,23 +13,23 @@ if (email === '') {
     alert('Canceled');
 } else if (email.length < number5) {
     alert('I don\'t know any emails having name length less than 5 symbols');
-} else if ((email !== user1) && (email !== user2)) {
+} else if (email !== user1 && email !== user2) {
     alert('I don’t know you');
-} else if ((email === user1) && (email === user2)) {
+} else if (email === user1 && email === user2) {
     alert(password)
-} else if ((password === '') || !(password)) {
+} else if (password === '' || !password) {
     alert('Canceled');
-} else if (!((password === pass1) && (email === user1)) && !((password === pass2) && (email === user2))) {
+} else if (!(password === pass1 && email === user1) && !(password === pass2 && email === user2)) {
     alert('Wrong password');
-} else if (!(confirm('Do you want to change your password?'))) {
+} else if (!confirm('Do you want to change your password?')) {
     alert('You have failed the change.');
 } else if (!(oldpass = prompt('Enter old password'))) {
     alert('Canceled');
-} else if (((oldpass === pass1) || (oldpass === pass2)) && !(newpass = prompt('Enter new password'))) {
+} else if ((oldpass === pass1 || oldpass === pass2) && !(newpass = prompt('Enter new password'))) {
     alert('Canceled')
 } else if (newpass.length < number6) {
     alert('It’s too short password. Sorry.');
-} else if ((prompt('Enter password for confirm')) !== (newpass)) {
+} else if (prompt('Enter password for confirm') !== newpass) {
     alert('You wrote the wrong password.')
 } else {
     alert('You have successfully changed your password.')
