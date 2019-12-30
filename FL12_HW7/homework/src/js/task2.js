686f9876c11d42f7d11c01f93f1305c6dd26225c
vvburkovsky <bvitaliy1@gmail.com>
2019-12-30 13:52:06 +0200

    start task 2

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,16 @@
 // Your code goes here
+if (!confirm('Do you want to play a game?')){
+    alert('You did not become a billionaire, but can.');
+}
+
+
+else {
+    do {
+        number = Math.floor(Math.random() * 9);
+        text = prompt('enter a number of pocket')
+
+        
+    }
+    while (exit)
+
+    }
