c862462c48c6caebc266d120d79f99099357533c
vvburkovsky <bvitaliy1@gmail.com>
2019-12-30 23:10:58 +0200

    some change

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -4,10 +4,14 @@ let user1 = 'admin@gmail.com';
 let user2 = 'user@gmail.com';
 let pass1 = 'AdminPass';
 let pass2 = 'UserPass';
+let number5 = 5;
+let number6 = 6;
+let oldpass;
+let newpass;
 
 if (email === '') {
     alert('Canceled');
-} else if (email.length < 5) {
+} else if (email.length < number5) {
     alert('I don\'t know any emails having name length less than 5 symbols');
 } else if ((email !== user1) && (email !== user2)) {
     alert('I don’t know you');
@@ -23,7 +27,7 @@ if (email === '') {
     alert('Canceled');
 } else if (((oldpass === pass1) || (oldpass === pass2)) && !(newpass = prompt('Enter new password'))) {
     alert('Canceled')
-} else if (newpass.length < 6) {
+} else if (newpass.length < number6) {
     alert('It’s too short password. Sorry.');
 } else if ((prompt('Enter password for confirm')) !== (newpass)) {
     alert('You wrote the wrong password.')
