0eb797bca3ce4911fa4e1baa0c8b3ea355c05476
vvburkovsky <bvitaliy1@gmail.com>
2019-12-30 23:19:35 +0200

    task 2 add variable

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,4 +1,11 @@
-// Your code goes here
+let range = 8;
+let chance;
+let prize;
+let currentprize;
+let number = 9;
+let text = 'Choose a roulette pocket number from 0 to ' + range + '\n Attempts left: ' + chance + '\n Total prize: ' + prize + '\n Possible prize on current attempt:' + currentprize + '\n';
+let exit;
+
 if (!confirm('Do you want to play a game?')){
     alert('You did not become a billionaire, but can.');
 }
