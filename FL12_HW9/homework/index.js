e18db4d8a35942673865464ed9d50db730f8028a
Vitaliy Burkovsky <bvitaliy1@gmail.com>
2020-01-14 18:02:47 +0200

    add task 6

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -6,48 +6,59 @@ let multi = [1, 2, 3];
 let min = [2, '5', 8];
 let filter = [2, 5, 8];
 let text = 'hey world';
+let datalist = [2, 7];
 
 function convert() {
-    var arr = [Number(one), String(two), String(three), Number(four)];
+    let arr = [Number(one), String(two), String(three), Number(four)];
     return arr;
-};
+}
 
 function multiply(data, value) {
-    var multiply = [];
+    let multiply = [];
     data.forEach(function (item) {
         multiply.push(item * value)
     });
     return multiply;
-};
+}
 
 function minus(data, value) {
-    var minus = [];
+    let minus = [];
     data.forEach(function (item) {
         minus.push(Number(item) + value)
     });
     return minus;
-};
+}
 
 function filtered(data, value) {
-    var multiply = [];
+    let multiply = [];
 
     data.forEach(function (item) {
-        if (data = (item % value) === 0) {
-            multiply.push(Number (item));
+        if (data = item % value === 0) {
+            multiply.push(Number(item));
         }
     });
 
     return multiply;
-};
+}
 
-function revers(data) {
-    return data.split("").reverse().join("");
+function flipOver(data) {
+    return data.split('').reverse().join('');
 
 }
+
+function revers(data) {
+    return data.split('').reverse().join('');
+
+}
+
+function makeListFromRange ([start, end]) {
+    return Array(end - start + Number(one)).fill().map((_, i) => start + i);
+  }
 
 
 console.log(convert(one, two, three, four));
 console.log(multiply(multi, 3));
 console.log(minus(min, 3));
 console.log(filtered(filter, 2));
-console.log(revers(text));
+console.log(flipOver(text));
+console.log(makeListFromRange(datalist));
