b066e88e5f7cc87c0997aa2ece0d8551d0f6d62d
Vitaliy Burkovsky <bvitaliy1@gmail.com>
2020-01-14 23:37:15 +0200

    last commit

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -9,14 +9,13 @@ let text = 'hey world';
 let datalist = [2, 7];
 let subs = [58, 14, 48, 2, 31, 29];
 let number = 30;
-
 const actors = [
     { name: 'tommy', age: 36 },
     { name: 'lee', age: 28 }
-  ];
-  
+];
 const date = new Date(2019, 0, 2);
 let seconddate = new Date('6/15/2018 09:15:00');
+
 
 function convert() {
     let arr = [Number(one), String(two), String(three), Number(four)];
@@ -40,28 +39,27 @@ function mapArray(data, value) {
 }
 
 function filterArray(data, value) {
-    let multiply = [];
+    let last = [];
 
     data.forEach(function (item) {
-        if (data = item % value === 0) {
-            multiply.push(Number(item));
+        data = item % value;
+        if (data === 0) {
+            last.push(Number(item));
         }
     });
-
-    return multiply;
+    return last;
 }
 
 function flipOver(data) {
     return data.split('').reverse().join('');
-
 }
 
-function makeListFromRange ([start, end]) {
+function makeListFromRange([start, end]) {
     return Array(end - start + Number(one)).fill().map((_, i) => start + i);
-  }
+}
 
 
-  function getArrayOfKeys(data) {
+function getArrayOfKeys(data) {
     let newactor = [];
     data.forEach(function (item) {
         newactor.push(item.name)
@@ -85,22 +83,19 @@ function substitute(data) {
 function getPastDay(data, month) {
     let dat = new Date(data);
     dat.setDate(dat.getDate() - month);
-
     return dat.getDate();
 }
 
-function formatDate (data) {
-    if (data == null){
+function formatDate(data) {
+    if (data === undefined) {
         let curdate = new Date();
         let date = curdate.getFullYear() + '/' + (curdate.getMonth() + 1) + '/' + curdate.getDate()
-        return date + ' ' + curdate.getHours() + ':' + curdate.getMinutes(); 
+        return date + ' ' + curdate.getHours() + ':' + curdate.getMinutes();
+    } else {
+        let inputdate = data;
+        let day = inputdate.getFullYear() + '/' + (inputdate.getMonth() + 1) + '/' + inputdate.getDate();
+        return day + ' ' + data.getHours() + ':' + data.getMinutes();
     }
-
-    else {
-       let inputdate = data;
-       let day = inputdate.getFullYear() + '/' + (inputdate.getMonth() + 1) + '/' + inputdate.getDate();
-       return day + ' ' + data.getHours() + ':' + data.getMinutes(); 
-    };
 }
 
 console.log(convert(one, two, three, four));
@@ -111,5 +106,8 @@ console.log(flipOver(text));
 console.log(makeListFromRange(datalist));
 console.log(getArrayOfKeys(actors));
 console.log(substitute(subs));
+console.log(getPastDay(date, 1));
+console.log(getPastDay(date, 2));
 console.log(getPastDay(date, 365));
+console.log(formatDate());
 console.log(formatDate(seconddate));
